597393944fd20c0bfbd85f1c192a87d09a4df299
dbrumley <dbrumley@forallsecure.com>
2025-07-10 19:25:51 -0400

    added as many ways as I could think of to fail

--- fuzz_target.cpp
+++ fuzz_target.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <stdexcept> 
+#include <stdexcept>
+#include <assert.h>
+#include <windows.h>
 
 
 void test_address_sanitizer_boo(std::string data) {
     // This function uses a null deref to see if mayhem catches it with 'address_sanitizer' 
     if (data.size() >= 3) {
         if (data.c_str()[0] == 'b' && data.c_str()[1] == 'o' && data.c_str()[2] == 'o') {
+            // Note: this will crash powershell in MSVC. 
             std::cout << "Found 'boo' address sanitizer corner case!\n";
             int* x = new int[100];
             x[100] = 5; 
@@ -17,7 +20,7 @@ void test_address_sanitizer_boo(std::string data) {
     }
 }
 
-void test_deref_mom(std::string data) {
+void test_null_deref_mom(std::string data) {
     // This function uses a null deref to see if mayhem catches it with 'mom' 
     if (data.size() >= 3) {
         if (data.c_str()[0] == 'm' && data.c_str()[1] == 'o' && data.c_str()[2] == 'm') {
@@ -37,6 +40,24 @@ void test_runtime_error_dad(std::string data) {
     }
 }
 
+void test_assert_cab(std::string data) {
+    if (data.size() >= 3) {
+        if (data.c_str()[0] == 'c' && data.c_str()[1] == 'a' && data.c_str()[2] == 'b') {
+            std::cout << "Found 'cab' assert corner case!\n";
+            assert(1 == 0);
+        }
+    }
+}
+
+void test_raise_fail_fast_dog(std::string data) {
+    if (data.size() >= 3) {
+        if (data.c_str()[0] == 'd' && data.c_str()[1] == 'o' && data.c_str()[2] == 'g') {
+            std::cout << "Found 'dog' RaiseFailFastException!\n";
+            RaiseFailFastException(nullptr, nullptr, 0);
+        }
+    }
+}
+
 void test_abort_set_behavior_set(std::string data) {
     // This function uses a null deref to see if mayhem catches it with 'set_behavior' 
     if (data.size() >= 3) {
@@ -44,8 +65,8 @@ void test_abort_set_behavior_set(std::string data) {
             std::cout << "Found 'set' set_abort_behavior + abort corner case!\n";
             //  abort() will normally pop-up a dialoge and then silently exit with code 3. 
             // This disables that behavior to simply crash.
-            _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT); 
-            abort(); // abort();
+            _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+            abort(); // abort(). You'll see this in the crash 
 
         }
     }
@@ -84,10 +105,12 @@ int main(int argc, char* argv[]) {
 	std::cout << "Read " << data.c_str() << " from input file.\n";
 
 	test_abort_bug(data);
-	test_deref_mom(data);
+	test_null_deref_mom(data);
 	test_runtime_error_dad(data);
 	test_abort_set_behavior_set(data);
 	test_address_sanitizer_boo(data);
+	test_raise_fail_fast_dog(data);
+	test_assert_cab(data);
 
     return 0;
 }
